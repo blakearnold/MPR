42d479a6800c2e69135e4119f0b9c4e9c3c634d6
Ben Collins <bcollins@ubuntu.com>
2007-06-18 19:45:25 -0400

    UBUNTU: ubuntu: Ok, really remove bigiron
    Signed-off-by: Ben Collins <bcollins@ubuntu.com>

--- debian/rules.d/i386.mk
+++ debian/rules.d/i386.mk
@@ -1,7 +1,7 @@
 build_arch	= i386
 header_arch	= x86_64
 defconfig	= defconfig
-flavours	= 386 generic server server-bigiron
+flavours	= 386 generic server
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
