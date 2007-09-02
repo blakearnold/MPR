c50ad1397a71f105174f360f212471bfe7f8f61e
Amit Kucheria <amit.kucheria@ubuntu.com>
2007-09-02 21:42:18 +0300

    UBUNTU: Move UME to a Custom build and add first setup of thermal framework
    patches
    
    UME is now a custom flavour under i386, allowing it to be patched seperately
    from other flavours.
    
    This patch adds the thermal framework for UME.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@ubuntu.com>

--- debian/rules.d/i386.mk
+++ debian/rules.d/i386.mk
@@ -1,7 +1,7 @@
 build_arch	= i386
 header_arch	= x86_64
 defconfig	= defconfig
-flavours	= 386 generic server ume
+flavours	= 386 generic server
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
@@ -10,4 +10,4 @@ do_debug_image	= true
 
 loader		= grub
 
-custom_flavours	= xen rt
+custom_flavours	= xen rt ume
