8e69a1897235a829de42cd3628dab9e9a612b27b
Rusty Russell <rusty@rustcorp.com.au>
2008-01-08 15:40:02 +0200

    virtio:net-fix-xmit-skb-free-real
    This fixes a potential dangling xmit problem.
    
    We also suppress refill interrupts until we need them.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -52,11 +52,13 @@ static inline void vnet_hdr_to_sg(struct scatterlist *sg, struct sk_buff *skb)
 	sg_init_one(sg, skb_vnet_hdr(skb), sizeof(struct virtio_net_hdr));
 }
 
-static void skb_xmit_done(struct virtqueue *rvq)
+static void skb_xmit_done(struct virtqueue *svq)
 {
-	struct virtnet_info *vi = rvq->vdev->priv;
+	struct virtnet_info *vi = svq->vdev->priv;
 
-	/* In case we were waiting for output buffers. */
+	/* Suppress further interrupts. */
+	svq->vq_ops->disable_cb(svq);
+	/* We were waiting for more output buffers. */
 	netif_wake_queue(vi->dev);
 }
 
@@ -208,10 +210,10 @@ again:
 	return received;
 }
 
-static void free_old_xmit_skbs(struct virtnet_info *vi)
+static unsigned free_old_xmit_skbs(struct virtnet_info *vi)
 {
 	struct sk_buff *skb;
-	unsigned int len;
+	unsigned int len, i = 0;
 
 	while ((skb = vi->svq->vq_ops->get_buf(vi->svq, &len)) != NULL) {
 		pr_debug("Sent skb %p\n", skb);
@@ -219,7 +221,9 @@ static void free_old_xmit_skbs(struct virtnet_info *vi)
 		vi->dev->stats.tx_bytes += len;
 		vi->dev->stats.tx_packets++;
 		kfree_skb(skb);
+		i++;
 	}
+	return i;
 }
 
 static int start_xmit(struct sk_buff *skb, struct net_device *dev)
@@ -234,8 +238,6 @@ static int start_xmit(struct sk_buff *skb, struct net_device *dev)
 	sg_init_table(sg, 1+MAX_SKB_FRAGS);
 
 	pr_debug("%s: xmit %p %s\n", dev->name, skb, print_mac(mac, dest));
-
-	free_old_xmit_skbs(vi);
 
 	/* Encode metadata header at front. */
 	hdr = skb_vnet_hdr(skb);
@@ -268,11 +270,26 @@ static int start_xmit(struct sk_buff *skb, struct net_device *dev)
 	vnet_hdr_to_sg(sg, skb);
 	num = skb_to_sgvec(skb, sg+1, 0, skb->len) + 1;
 	__skb_queue_head(&vi->send, skb);
+
+again:
 	err = vi->svq->vq_ops->add_buf(vi->svq, sg, num, 0, skb);
 	if (err) {
+		/* Can we free any used skbs? */
+		if (free_old_xmit_skbs(vi))
+			goto again;
+
 		pr_debug("%s: virtio not prepared to send\n", dev->name);
-		skb_unlink(skb, &vi->send);
 		netif_stop_queue(dev);
+
+		/* Activate callback for using skbs: if this fails it
+		 * means some were used in the meantime. */
+		if (unlikely(!vi->svq->vq_ops->enable_cb(vi->svq))) {
+			printk("Unlikely: restart svq failed\n");
+			netif_start_queue(dev);
+			goto again;
+		}
+		__skb_unlink(skb, &vi->send);
+
 		return NETDEV_TX_BUSY;
 	}
 	vi->svq->vq_ops->kick(vi->svq);
