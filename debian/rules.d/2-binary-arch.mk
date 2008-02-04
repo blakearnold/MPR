7a55cce85477ea989357bc0a07820db884277eeb
Tim Gardner <tim.gardner@canonical.com>
2008-02-04 14:21:13 -0700

    UBUNTU: Remove all ko files any time the configs change. Ignore: yes
    This helps to detect missing modules, especially when a config option
    turns off a module after a test build and the developer is too lazy to
    rebuild from scratch (besides, its takes awhile).
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -10,6 +10,7 @@ $(stampdir)/stamp-prepare-%: $(confdir)/config $(confdir)/config.%
 	@echo "Preparing $*..."
 	install -d $(builddir)/build-$*
 	cat $^ | sed -e 's/.*CONFIG_VERSION_SIGNATURE.*/CONFIG_VERSION_SIGNATURE="Ubuntu $(release)-$(revision)-$*"/' > $(builddir)/build-$*/.config
+	find $(builddir)/build-$* -name "*.ko" | xargs rm
 	$(kmake) O=$(builddir)/build-$* silentoldconfig prepare scripts
 	touch $@
 
