11c3136528b526dc4c17ce64b25d18a4c967fca1
Mario Limonciello <supermario@mlimonciello.(none)>
2008-04-04 15:51:26 -0500

    UBUNTU: Enable Reset and SCO workaround on Dell 410 BT adapter
    OriginalAuthor: Mario Limonciello
    Ignore: no
    
    When the Dell 410 BT adapter is put into BT radio mode, it needs
    to be prepared like many other Broadcom adapters.
    
    Signed-off-by: Mario Limonciello <Mario_Limonciello@Dell.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/bluetooth/hci_usb.c
+++ drivers/bluetooth/hci_usb.c
@@ -134,6 +134,8 @@ static struct usb_device_id blacklist_ids[] = {
 	{ USB_DEVICE(0x413c, 0x8126), .driver_info = HCI_RESET | HCI_WRONG_SCO_MTU },
 	/* Dell Wireless 370 */
 	{ USB_DEVICE(0x413c, 0x8156), .driver_info = HCI_RESET | HCI_WRONG_SCO_MTU },
+	/* Dell Wireless 410 */
+	{ USB_DEVICE(0x413c, 0x8152), .driver_info = HCI_RESET | HCI_WRONG_SCO_MTU },
 
 	/* Microsoft Wireless Transceiver for Bluetooth 2.0 */
 	{ USB_DEVICE(0x045e, 0x009c), .driver_info = HCI_RESET },
