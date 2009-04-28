dc097ada3df14d0374406584f1427a8344a437b6
Bryan Wu <bryan.wu@canonical.com>
2009-04-28 23:32:07 +0800

    bluetooth hid: enable quirk handling for Apple Wireless Keyboards in 2.6.24
    Bug: #227501
    BugLink: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/227501
    
    This patch is basically a backport of
    commit ee8a1a0a1a5817accd03ced7e7ffde3a4430f485 upstream
    and 5a027e910cc26f9b1f8e32daaeb0d552664b2a54 in Intrepid kernel
    which was made after the big HID overhaul in 2.6.28.
    
    Kernel 2.6.24 fails to handle quirks for the aluminum Apple Wireless
    Keyboard because it is handled as USB device and not as Bluetooth
    device. This patch expands 'hidp_blacklist' to make the kernel handle
    the keyboard in the same way as the Apple wireless Mighty Mouse (also a
    Bluetooth device).
    
    Signed-off-by: Torsten Rausche <torsten@rausche.net>
    Cc: Jan Scholz <Scholz@fias.uni-frankfurt.de>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Bryan Wu <bryan.wu@canonical.com>
    Acked-by: Amit Kucheria <amit.kucheria@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -696,6 +696,8 @@ static const struct {
 	{ 0x05ac, 0x030c, HID_QUIRK_MIGHTYMOUSE | HID_QUIRK_INVERT_HWHEEL },
 	/* Apple Wireless Aluminum Keyboard */
 	{ 0x05ac, 0x022c, HID_QUIRK_APPLE_HAS_FN },
+	{ 0x05ac, 0x022d, HID_QUIRK_APPLE_HAS_FN },
+	{ 0x05ac, 0x022e, HID_QUIRK_APPLE_HAS_FN },
 
 	{ }	/* Terminating entry */
 };
