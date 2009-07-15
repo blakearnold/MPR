cd2c96353c94d9b805d2a149c7b7c73403f7c9eb
Stefan Bader <stefan.bader@canonical.com>
2009-07-15 18:04:38 +0200

    Revert "UBUNTU: SAUCE: Reduce AACRAID hardware queue size"
    This reverts commit 0fc2669e62b5d808f9d5cf514823860f566042cd.
    
    There has been some doubt this is the right solution as it seems to be
    firmware version related. Upstream had proposed a different solution,
    so we take this back.
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Andy Whitcroft <apw@canonical.com>

--- drivers/scsi/aacraid/aacraid.h
+++ drivers/scsi/aacraid/aacraid.h
@@ -24,7 +24,7 @@
 #define AAC_MAX_LUN		(8)
 
 #define AAC_MAX_HOSTPHYSMEMPAGES (0xfffff)
-#define AAC_MAX_32BIT_SGBCOUNT	((unsigned short)127)
+#define AAC_MAX_32BIT_SGBCOUNT	((unsigned short)256)
 
 /*
  * These macros convert from physical channels to virtual channels
